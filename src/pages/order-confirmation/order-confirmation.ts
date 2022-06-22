693ccdeb43338da0f8263975bec996a55b378793
DenisWm <denismamoni@hotmail.com>
2022-06-22 09:29:59 -0400

    salvando pedido

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -6,6 +6,7 @@ import { EnderecoDTO } from '../../models/endereco.dto';
 import { PedidoDTO } from '../../models/pedido.dto';
 import { CartService } from '../../services/domain/cart.service';
 import { ClienteService } from '../../services/domain/cliente.service';
+import { PedidoService } from '../../services/domain/pedido.service';
 
 @IonicPage()
 @Component({
@@ -19,7 +20,7 @@ export class OrderConfirmationPage {
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public cartService: CartService, public clienteService: ClienteService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public cartService: CartService, public clienteService: ClienteService, public pedidoService: PedidoService) {
     this.pedido = this.navParams.get('pedido');
   }
 
@@ -43,5 +44,24 @@ export class OrderConfirmationPage {
 
   total() {
     return this.cartService.total();
+  }
+
+  back(){
+    this.navCtrl.setRoot('CartPage');
+  }
+  checkout(){
+    this.pedidoService.insert(this.pedido)
+    .subscribe(response =>{
+      console.log(this.pedido);
+      this.cartService.createrOrClearCart();
+      console.log(response.headers.get('location'));
+      
+    }, error=>{
+      if(error.status == 403) {
+        this.navCtrl.setRoot('HomePage');
+      }
+    });
+    
+
   }
 }
