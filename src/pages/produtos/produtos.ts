3a3a269e3a7785c8b17d913b72adf8e346a6a3db
DenisWm <denismamoni@hotmail.com>
2022-06-22 14:55:05 -0400

    Refresher

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -24,7 +24,11 @@ export class ProdutosPage {
   }
 
   ionViewDidLoad() {
+  this.loadData();  
+    
+  }
 
+  loadData(){
     let categoria_id = this.navParams.get('categoria_id');
     let loader = this.presentLoading();
 
@@ -60,5 +64,12 @@ export class ProdutosPage {
     });
     loader.present();
     return loader;
+  }
+
+  doRefresh(refresher){
+    this.loadData();
+    setTimeout(() => {
+      refresher.complete();
+    }, 1000);
   }
 }
