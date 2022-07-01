14a7d2b2d6255600b1e324b3799de6b493c788c9
DenisWm <denismamoni@hotmail.com>
2022-07-01 10:35:09 -0400

    Ajustes na camera

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { CameraOptions, Camera } from '@ionic-native/camera';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { API_CONFIG } from '../../config/api.config';
@@ -17,8 +18,17 @@ export class ProfilePage {
   cliente: ClienteDTO;
   picture: string;
   cameraOn: boolean = false;
+  profileImage;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public storage: StorageService, public clienteService: ClienteService, public camera: Camera) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams, 
+    public storage: StorageService, 
+    public clienteService: ClienteService, 
+    public camera: Camera, 
+    public sanitizer: DomSanitizer) {
+
+      this.profileImage = 'assets/imgs/avatar-blank.png';
   }
 
   ionViewDidLoad() {
@@ -48,8 +58,23 @@ export class ProfilePage {
     this.clienteService.getImageFromBucket(this.cliente.id)
     .subscribe(response => {
       this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+      this.blobToDataURL(response).then(dataUrl =>{
+        let str : string = dataUrl as string;
+        this.profileImage = this.sanitizer.bypassSecurityTrustUrl(str);
+      });
     },
-    error =>{});
+    error =>{
+      this.profileImage = 'assets/imgs/avatar-blank.png';
+    });
+  }
+
+  blobToDataURL(blob){
+    return new Promise((fulfill, reject) => {
+      let reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = (e) => fulfill(reader.result);
+      reader.readAsDataURL(blob);
+    })
   }
 
   getCameraPicture() {
@@ -95,7 +120,7 @@ export class ProfilePage {
     this.clienteService.uploadPicture(this.picture)
     .subscribe(response => {
       this.picture = null;
-      this.loadData();
+      this.getImageIfExists();
 
     }, erro =>{
       this.cameraOn = false;
